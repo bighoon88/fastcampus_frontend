32dfc9f07235f7f5fdc6e6a687d1d3bc1093dbf6
bighoon88 <rlaxognsa2@gmail.com>
2020-07-30 12:25:42 +0900

    02 변수

---
+++ 모던자바스크립트-JavaScript입문/02_변수/index.js
@@ -0,0 +1,27 @@
+//변수
+let value = 1;
+console.log(value);
+value = 2;
+console.log(value);
+//상수
+const a = 1;
+console.log(a);
+//데이터타입
+//숫자
+let value1 = 1;
+console.log(value1);
+//문자열
+let text = "hello";
+let name = "헬로우 자바스크립트";
+console.log(text);
+console.log(name);
+//boolean
+let good = true;
+let loading = false;
+console.log(good);
+console.log(loading);
+//null, undefined
+let good1 = null;
+let loading1 = undefined;
+console.log(good1);
+console.log(loading1);
