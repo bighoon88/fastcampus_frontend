f70b6b2b405d183f79199b2352d44ffd9240be24
bighoon88 <rlaxognsa2@gmail.com>
2020-07-30 12:34:10 +0900

    03 연산자 - 산술연산자, 대입연산자

---
+++ 모던자바스크립트-JavaScript입문/03_연산자-산술연산자,대입연산자/index.js
@@ -0,0 +1,16 @@
+//산술연산자
+let a = 1 + 2 - ( 3 * 4 ) / 4;
+console.log(a);
+let a1 = 1;
+console.log(a1++);
+console.log(a1);
+console.log(++a1);
+console.log(a1);
+
+//대입연산자
+let a2 = 3;
+a2 += 3;
+a2 -= 3;
+a2 *= 3;
+a2 /= 3;
+console.log(a2);
