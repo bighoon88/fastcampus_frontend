ff81005e3f472f00bd21e18264cf5a9dc8a644f0
bighoon88 <rlaxognsa2@gmail.com>
2020-07-30 18:47:21 +0900

    18 반복문 - while

---
+++ 모던자바스크립트-JavaScript입문/18_반복문-while/index.js
@@ -0,0 +1,15 @@
+//반복문
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+};
+
+let isFun = false;
+while (isFun === false) {
+  console.log(i);
+  i++;
+  if (i === 30) {
+    isFun = true;
+  };
+};
