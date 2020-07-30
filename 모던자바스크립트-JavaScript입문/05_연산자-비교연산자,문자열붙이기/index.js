9a317038d4669337a356752d7cbec612ad7d0e21
bighoon88 <rlaxognsa2@gmail.com>
2020-07-30 16:16:06 +0900

    05 연산자 - 비교연산자, 문자열 붙이기

---
+++ 모던자바스크립트-JavaScript입문/05_연산자-비교연산자,문자열붙이기/index.js
@@ -0,0 +1,22 @@
+//비교연산자
+const a = 1;
+const b = 1;
+const equals = a === b;
+console.log(equals);
+
+const notEquals = a !== b;
+console.log(notEquals);
+
+const a1 = 10;
+const b1 = 15;
+const c1 = 15;
+console.log(a1 < b1);
+console.log(b1 > a1);
+console.log(b1 >= a1);
+console.log(a1 <= c1);
+console.log(b1 < c1);
+
+//문자열 붙이기
+const a2 = "안녕";
+const b2 = "하세요";
+console.log(a2 + b2);
